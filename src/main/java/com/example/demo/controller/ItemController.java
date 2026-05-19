d73f22110a3bb151f87c991ba50682ca8d675375
student <student@FJ-1025>
2026-05-19 15:10:42 +0900

    家計簿一覧へ項目追加機能を実装

--- src/main/java/com/example/demo/controller/ItemController.java
+++ src/main/java/com/example/demo/controller/ItemController.java
@@ -1,30 +1,69 @@
 package com.example.demo.controller;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.entity.Item;
+import com.example.demo.repository.GenreRepository;
 import com.example.demo.repository.ItemRepository;
+import com.example.demo.repository.UserRepository;
 
 @Controller
 public class ItemController {
 
 	private final ItemRepository itemRepository;
+	private final GenreRepository genreRepository;
+	private final UserRepository userRepository;
 	
-	public ItemController(ItemRepository itemRepository) {
+	public ItemController(ItemRepository itemRepository, GenreRepository genreRepository, UserRepository userRepository) {
 		this.itemRepository = itemRepository;
+		this.genreRepository = genreRepository;
+		this.userRepository = userRepository;
 	}
 	
 	//商品一覧
-	@GetMapping("items")
+	@GetMapping("/items")
 	public String index(Model model) {
 		List<Item> itemList = itemRepository.findAll();
 		model.addAttribute("items", itemList);
 		
 		return "items";
 	}
+	
+	//項目追加画面の表示
+	@GetMapping("/items/add")
+	public String add() {
+		return "addItem";
+	}
+	
+	//項目追加機能
+	@PostMapping("/items/add")
+	public String store(
+		@RequestParam LocalDate addDate,
+		@RequestParam String itemName,
+		@RequestParam Integer genreId,
+		@RequestParam Integer price,
+		@RequestParam String comment) {
+		
+		Item item = new Item(
+				userRepository.findById(1).get(),
+				addDate, 
+				itemName, 
+				genreRepository.findById(genreId).get(),
+				price, 
+				comment);
+		itemRepository.save(item);
+		return "redirect:/items";
+	}
+		
+	
+	
+	
 
 }
--- src/main/java/com/example/demo/entity/Item.java
+++ src/main/java/com/example/demo/entity/Item.java
@@ -12,92 +12,92 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-	@Table(name = "items")
-	public class Item{
-		@Id
-		@GeneratedValue(strategy = GenerationType.IDENTITY)
-		private Integer id;
-		
-		@Column(name = "item_name")
-		private String itemName;
-		
-		@ManyToOne
-		@JoinColumn(name = "user_id")
-		private User user;
-		
-		@ManyToOne
-		@JoinColumn(name = "genre_id")
-		private Genre genre;
-		
-		private Integer price;
-		private LocalDate addDate;
-		private String comment;
-		
-		public Item() {
-		}
+@Table(name = "items")
+public class Item {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 
-		public Item(String itemName, User user, Genre genre, Integer price, LocalDate addDate, String comment) {
-			super();
-			this.itemName = itemName;
-			this.user = user;
-			this.genre = genre;
-			this.price = price;
-			this.addDate = addDate;
-			this.comment = comment;
-		}
+	@Column(name = "item_name")
+	private String itemName;
 
-		//ゲッターｔセッター
-		public String getItemName() {
-			return itemName;
-		}
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
 
-		public void setItemName(String itemName) {
-			this.itemName = itemName;
-		}
+	@ManyToOne
+	@JoinColumn(name = "genre_id")
+	private Genre genre;
 
-		public User getUser() {
-			return user;
-		}
+	private Integer price;
+	private LocalDate addDate;
+	private String comment;
 
-		public void setUser(User user) {
-			this.user = user;
-		}
-
-		public Genre getGenre() {
-			return genre;
-		}
-
-		public void setGenre(Genre genre) {
-			this.genre = genre;
-		}
-
-		public Integer getPrice() {
-			return price;
-		}
-
-		public void setPrice(Integer price) {
-			this.price = price;
-		}
-
-		public LocalDate getAddDate() {
-			return addDate;
-		}
-
-		public void setAddDate(LocalDate addDate) {
-			this.addDate = addDate;
-		}
-
-		public String getComment() {
-			return comment;
-		}
-
-		public void setComment(String comment) {
-			this.comment = comment;
-		}
-
-		public Integer getId() {
-			return id;
-		}
-		
-		
+	//コンストラクタ
+	public Item() {
 	}
+
+	public Item(User user, LocalDate addDate, String itemName,Genre genreName, Integer price,  String comment) {
+		super();
+		this.user = user;
+		this.addDate = addDate;
+		this.itemName = itemName;
+		this.genre = genreName;
+		this.price = price;
+		this.comment = comment;
+	}
+
+	//ゲッターｔセッター
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Genre getGenre() {
+		return genre;
+	}
+
+	public void setGenre(Genre genre) {
+		this.genre = genre;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+	public LocalDate getAddDate() {
+		return addDate;
+	}
+
+	public void setAddDate(LocalDate addDate) {
+		this.addDate = addDate;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+}
